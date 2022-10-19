9d1489434e890e31823d8b4a19e0b66304c1d266
Do Phuong <9a3.c2phuongliet.thephuong@gmail.com>
2022-10-19 17:31:24 +0700

    grid tigren

---
+++ app/code/Tigren/Grid/Plugin/AddAttributesToUiDataProvider.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\Grid\Plugin;
+
+use Dev\Grid\Ui\DataProvider\Category\ListingDataProvider as CategoryDataProvider;
+use Magento\Eav\Api\AttributeRepositoryInterface;
+use Magento\Framework\App\ProductMetadataInterface;
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+
+/**
+ * Class AddAttributesToUiDataProvider
+ * @package Dev\Grid\Plugin
+ */
+class AddAttributesToUiDataProvider
+{
+    /** @var AttributeRepositoryInterface */
+    private $attributeRepository;
+
+    /** @var ProductMetadataInterface */
+    private $productMetadata;
+
+    /**
+     * Constructor
+     *
+     * @param AttributeRepositoryInterface $attributeRepository
+     * @param ProductMetadataInterface $productMetadata
+     */
+    public function __construct(
+        AttributeRepositoryInterface $attributeRepository,
+        ProductMetadataInterface     $productMetadata
+    )
+    {
+        $this->attributeRepository = $attributeRepository;
+        $this->productMetadata = $productMetadata;
+    }
+
+    /**
+     * Get Search Result after plugin
+     *
+     * @param CategoryDataProvider $subject
+     * @param SearchResult $result
+     * @return SearchResult
+     */
+    public function afterGetSearchResult(CategoryDataProvider $subject, SearchResult $result)
+    {
+        if ($result->isLoaded()) {
+            return $result;
+        }
+
+        $edition = $this->productMetadata->getEdition();
+
+        $column = 'entity_id';
+
+        if ($edition == 'Enterprise') {
+            $column = 'row_id';
+        }
+
+        $attribute = $this->attributeRepository->get('catalog_category', 'name');
+
+        $result->getSelect()->joinLeft(
+            ['devgridname' => $attribute->getBackendTable()],
+            'devgridname.' . $column . ' = main_table.' . $column . ' AND devgridname.attribute_id = '
+            . $attribute->getAttributeId(),
+            ['name' => 'devgridname.value']
+        );
+
+        $result->getSelect()->where('devgridname.value LIKE "B%"');
+
+        return $result;
+    }
+}
